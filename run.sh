de1d90d861d6d23e6913e29785239068ed31dbe0
Traian <ntvidrascu@gmail.com>
2020-03-26 00:03:49 +0100

    fixed bug

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@
 #SBATCH --cpus-per-task=3
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=2:00:00
-#SBATCH --mem=64G
-#SBATCH --gres=gpu:titanv:1
-#SBATCH -p gpu_titan
+#SBATCH --mem=60000M
+#SBATCH --gres=gpu:1
+#SBATCH -p gpu
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=traian.vidrascu@student.uva.nl
 
