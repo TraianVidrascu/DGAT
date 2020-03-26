88b79e4d2d8884698503616a8fb68593aed8066f
Traian <ntvidrascu@gmail.com>
2020-03-26 12:52:13 +0100

    train encoder and decoder script

---
+++ run_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=DGAT
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=4:30:00
+#SBATCH --mem=90000M
+#SBATCH --gres=gpu:1
+#SBATCH -p gpu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=traian.vidrascu@student.uva.nl
+
+conda activate dgat
+python encoder.py
+python train_decoder.py
