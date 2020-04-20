5251b49d07388b3062ccb291c801c26e6f822814
Traian <ntvidrascu@gmail.com>
2020-04-20 17:34:19 +0200

    train encoder refactoring

--- train_encoder.py
+++ train_encoder.py
@@ -42,6 +42,8 @@ def get_encoder(args, x_size, g_size):
 
 
 def train_encoder(args, model, data_loader):
+    wandb.watch(model, log="all")
+
     # system parameters
     dev = args.device
     eval = args.eval
@@ -54,6 +56,7 @@ def train_encoder(args, model, data_loader):
     batch_size = args.batch
     negative_ratio = args.negative_ratio
 
+    # encoder save file path
     dataset_name = data_loader.get_name()
     encoder_file = ENCODER + '_' + args.model.lower() + '_' + dataset_name.lower() + '.pt'
 
@@ -61,10 +64,17 @@ def train_encoder(args, model, data_loader):
     x, g, graph = data_loader.load_train('cpu')
     n = x.shape[0]
 
-    wandb.watch(model, log="all")
+    # load graph base structure
+    edge_idx, edge_type = data_loader.graph2idx(graph, dev='cpu')
+    # load train edges
+    train_idx, train_type = edge_idx.clone(), edge_type.clone()
+    # load invalid negative sampling for train set
+    train_head_invalid_sampling, train_tail_invalid_sampling = data_loader.load_invalid_sampling(fold='train')
 
-    _, _, vaild_graph = data_loader.load_valid('cpu')
-    valid_idx, valid_type = data_loader.graph2idx(vaild_graph)
+    # load validation set for model metric
+    _, _, valid_graph = data_loader.load_valid('cpu')
+    valid_idx, valid_type = data_loader.graph2idx(valid_graph)
+    # load invalid negative sampling for valid set
     valid_head_invalid_sampling, valid_tail_invalid_sampling = data_loader.load_invalid_sampling('valid')
     valid_pos_edge_epoch_idx, valid_neg_edge_idx, valid_pos_edge_epoch_type = data_loader.negative_samples(n, valid_idx,
                                                                                                            valid_type,
@@ -72,65 +82,57 @@ def train_encoder(args, model, data_loader):
                                                                                                            valid_head_invalid_sampling,
                                                                                                            valid_tail_invalid_sampling,
                                                                                                            'cpu')
-    first = 0
 
+    # optimizer and scheduler for training
     optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=decay)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, last_epoch=-1, gamma=0.5)
 
-    train_idx, train_type = data_loader.graph2idx(graph, dev='cpu')
-
-    head_invalid_sampling, tail_invalid_sampling = data_loader.load_invalid_sampling()
-    edge_idx, edge_type = train_idx[:, :], train_type[:]
-
-    for epoch in range(first, epochs):
+    for epoch in range(epochs):
         s_epoch = time.time()
         model.train()
 
-        # # shuffling positive edges
-        s_shuffling = time.time()
+        # shuffling epoch training data
         perm = torch.randperm(edge_idx.shape[1])
         edge_idx = edge_idx[:, perm]
         edge_type = edge_type[perm]
-        head_invalid_sampling = head_invalid_sampling[perm]
-        tail_invalid_sampling = tail_invalid_sampling[perm]
-        t_shuffling = time.time()
-
-        # negative sampling and arranging data
-        s_sampling = time.time()
-        pos_edge_epoch_idx, neg_edge_idx, pos_edge_epoch_type = data_loader.negative_samples(n, edge_idx, edge_type,
-                                                                                             negative_ratio,
-                                                                                             head_invalid_sampling,
-                                                                                             tail_invalid_sampling,
-                                                                                             'cpu')
-        t_sampling = time.time()
+        train_head_invalid_sampling = train_head_invalid_sampling[perm]
+        train_tail_invalid_sampling = train_tail_invalid_sampling[perm]
 
         losses_epoch = []
-        m = pos_edge_epoch_idx.shape[1]
+        m = train_idx.shape[1]  # all train edges
         for itt in range(0, m, batch_size):
             s_batch = time.time()
-            # establish batch limit
+            # get batch boundaries
             start = itt
             end = itt + batch_size
 
-            # forward pass the model; getting the node embeddings
+            # get batch data
+            batch_idx = train_idx[:, start:end]
+            batch_type = train_type[start:end]
+            batch_head_invalid_sampling = train_head_invalid_sampling[start:end]
+            batch_tail_invalid_sampling = train_tail_invalid_sampling[start:end]
+
+            # get positive and negative samples for batch
+            s_sampling = time.time()
+            batch_pos_idx, batch_neg_idx, batch_type = data_loader.negative_samples(n, batch_idx, batch_type,
+                                                                                    negative_ratio,
+                                                                                    batch_head_invalid_sampling,
+                                                                                    batch_tail_invalid_sampling,
+                                                                                    'cpu')
+            t_sampling = time.time()
+
+            # forward pass the model; getting the node embeddings out of the structural information
             s_forward = time.time()
-            h_prime, g_prime = model(x.to(dev), g.to(dev), train_idx.to(dev), train_type.to(dev))
+            h_prime, g_prime = model(x.to(dev), g.to(dev), edge_idx.to(dev), edge_type.to(dev))
             t_forward = time.time()
 
-            # getting batch
-            s_slicing = time.time()
-            pos_edge_idx_batch = pos_edge_epoch_idx[:, start:end]
-            pos_edge_type_batch = pos_edge_epoch_type[start:end]
-            neg_edge_idx_batch = neg_edge_idx[:, start:end]
-            neg_edge_type_batch = pos_edge_epoch_type[start:end]
-            t_slicing = time.time()
-
+            # compute model loss for positive and negative samples
             s_loss = time.time()
             loss = model.loss(h_prime, g_prime,
-                              pos_edge_idx_batch.to(dev),
-                              pos_edge_type_batch.to(dev),
-                              neg_edge_idx_batch.to(dev),
-                              neg_edge_type_batch.to(dev))
+                              batch_pos_idx.to(dev),
+                              batch_type.to(dev),
+                              batch_neg_idx.to(dev),
+                              batch_type.to(dev))
             t_loss = time.time()
 
             torch.cuda.empty_cache()
@@ -148,15 +150,15 @@ def train_encoder(args, model, data_loader):
 
             if args.debug == 1:
                 print('Batch time: %.2f ' % (t_batch - s_batch) +
+                      'Sampling time: %.4f ' % (t_sampling - s_sampling) +
                       'Forward time: %.2f ' % (t_forward - s_forward) +
-                      'Slicing time: %.2f ' % (t_slicing - s_slicing) +
                       'Loss time: %.2f ' % (t_loss - s_loss) +
                       'Optim time: %.2f ' % (t_optim - s_optim) +
                       'Loss Batch: %.4f ' % (losses_epoch[-1]))
 
         loss_epoch = sum(losses_epoch) / len(losses_epoch)
 
-        # validation loss
+        # compute validation loss
         model.eval()
         h_valid, g_valid = model(x.to(dev), g.to(dev), train_idx.to(dev), train_type.to(dev))
         valid_loss = model.loss(h_valid, g_valid, valid_pos_edge_epoch_idx.to(dev), valid_pos_edge_epoch_type.to(dev),
@@ -168,8 +170,6 @@ def train_encoder(args, model, data_loader):
         t_epcoh = time.time()
         if args.debug == 1:
             print('Epoch time: %.4f ' % (t_epcoh - s_epoch) +
-                  'Sampling time: %.4f ' % (t_sampling - s_sampling) +
-                  'Shuffling time: %.4f ' % (t_shuffling - s_shuffling) +
                   'Loss Epoch: %.4f ' % loss_epoch +
                   'Loss Valid: %.4f ' % valid_loss)
 
@@ -220,12 +220,12 @@ def main():
 
     # system parameters
     parser.add_argument("--device", type=str, default='cuda', help="Device to use for training.")
-    parser.add_argument("--eval", type=int, default=500, help="After how many epochs to evaluate.")
-    parser.add_argument("--debug", type=int, default=0, help="Debugging mod.")
+    parser.add_argument("--eval", type=int, default=1000, help="After how many epochs to evaluate.")
+    parser.add_argument("--debug", type=int, default=1, help="Debugging mod.")
 
     # training parameters
-    parser.add_argument("--epochs", type=int, default=500, help="Number of training epochs for encoder.")
-    parser.add_argument("--step_size", type=int, default=500, help="Step size of scheduler.")
+    parser.add_argument("--epochs", type=int, default=1000, help="Number of training epochs for encoder.")
+    parser.add_argument("--step_size", type=int, default=250, help="Step size of scheduler.")
     parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate.")
     parser.add_argument("--decay", type=float, default=1e-3, help="L2 normalization weight decay encoder.")
     parser.add_argument("--dropout", type=float, default=0.3, help="Dropout for training.")
