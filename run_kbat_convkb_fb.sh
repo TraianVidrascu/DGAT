fcb741222f99e8903fa1ccf633aec68b763d0f2f
Traian <ntvidrascu@gmail.com>
2020-05-04 13:51:27 +0200

    scripts

--- run_kbat_convkb_fb.sh
+++ run_kbat_convkb_fb.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=KBAT_ConvKB_fb
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=3
-#SBATCH --time=27:00:00
+#SBATCH --time=20:00:00
 #SBATCH --mem=60000M
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu_shared
@@ -10,5 +10,5 @@
 #SBATCH --mail-user=traian.vidrascu@student.uva.nl
 
 conda activate dgat
-python train_decoder.py --debug 0 --model KBAT --dropout 0.3 --channels 50 --batch_size 128 --negative-ratio 40 --eval 75 --epochs 150 --dataset FB15K-237
+python train_decoder.py --debug 0 --model KBAT --dropout 0.3 --channels 50 --batch_size 128 --negative-ratio 40 --eval 200 --epochs 200 --dataset FB15K-237
 
