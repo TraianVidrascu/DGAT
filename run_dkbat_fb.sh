3b66a3d8ed42a3f29163c8660e668ee12d03b620
Traian <ntvidrascu@gmail.com>
2020-05-19 01:07:45 +0200

    running time

--- run_dkbat_fb.sh
+++ run_dkbat_fb.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=DKBAT_fb15k
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=10:00:00
+#SBATCH --time=48:00:00
 #SBATCH --mem=60000M
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu_shared
