62a3c454a3bf024d00342fb91f3e4bd10ee42cd4
Traian <ntvidrascu@gmail.com>
2020-05-19 22:24:50 +0200

    arguments

--- train_encoder.py
+++ train_encoder.py
@@ -313,8 +313,8 @@ def main():
     # encoder parameters
     parser.add_argument("--negative_slope", type=float, default=0.2, help="Negative slope for Leaky Relu")
     parser.add_argument("--heads", type=int, default=2, help="Number of heads per layer")
-    parser.add_argument("--output_encoder", type=int, default=400, help="Number of neurons per output layer")
-    parser.add_argument("--model", type=str, default=KBAT, help='Model name')
+    parser.add_argument("--output_encoder", type=int, default=200, help="Number of neurons per output layer")
+    parser.add_argument("--model", type=str, default=DKBAT, help='Model name')
 
     parser.add_argument("--channels", type=int, default=50, help="channels for decoder")
 
