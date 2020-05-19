61b57e92e946ed66a207ff69e724087c8f3e34a9
Traian <ntvidrascu@gmail.com>
2020-05-19 02:45:47 +0200

    paths

--- run_dkbat_wn.sh
+++ run_dkbat_wn.sh
@@ -11,4 +11,4 @@
 
 
 conda activate dgat
-python train_encoder.py --eval 1000  --use_paths 0 --use_partial 0 --debug 0 --model DKBAT --dataset WN18RR --margin 5 --batch 2000 --negative_ratio 4 --epochs 1000 --step_size 250 --decay 5e-6
+python train_encoder.py --eval 1000  --use_paths 0 --use_partial 0 --debug 0 --model DKBAT --dataset WN18RR --margin 5 --batch 1000 --negative_ratio 4 --epochs 1000 --step_size 250 --decay 5e-6
