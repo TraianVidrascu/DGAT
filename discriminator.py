backprop rel and discriminator fix

discriminator.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch_scatter import scatter_mean, scatter_add
 
+from torch.distributions import Normal
+
 
 class Discriminator(nn.Module):
     def __init__(self, x_size, g_size, embedding_size, hidden_size, dev='cpu'):
@@ -16,14 +18,16 @@ class Discriminator(nn.Module):
         self.fc3_rel = nn.Linear(hidden_size, 1, bias=True)
 
         self.dev = dev
-        self.to(dev)
         self.init_params()
 
         self.x_size = x_size
         self.g_size = g_size
         self.embedding_size = embedding_size
 
+        self.distribution = Normal(torch.zeros(self.embedding_size).float(), torch.ones(self.embedding_size).float())
+
         self.loss_fct = nn.BCELoss()
+        self.to(dev)
 
     def init_params(self):
         nn.init.xavier_normal_(self.fc1_entity.weight, gain=1.414)
@@ -45,35 +49,36 @@ class Discriminator(nn.Module):
     def pred_entity(self, h):
         h = self.fc1_entity(h)
         h = F.elu(h)
-        h = self.fc2_entity(h)
-        h = F.elu(h)
+        # h = self.fc2_entity(h)
+        # h = F.elu(h)
         h = self.fc3_entity(h)
         pred_h = torch.sigmoid(h)
         return pred_h.squeeze()
 
     def pred_rel(self, g):
-        g = self.fc1_rel(g)
+        g = self.fc1_entity(g)
         g = F.elu(g)
-        g = self.fc2_rel(g)
-        g = F.elu(g)
-        g = self.fc3_rel(g)
+        # g = self.fc2_entity(g)
+        # g = F.elu(g)
+        g = self.fc3_entity(g)
         pred_g = torch.sigmoid(g)
         return pred_g.squeeze()
 
     def forward(self, h, g):
         h = self.fc1_entity(h)
         h = F.elu(h)
-        h = self.fc2_entity(h)
-        h = F.elu(h)
+        # h = self.fc2_entity(h)
+        # h = F.elu(h)
         h = self.fc3_entity(h)
         pred_h = torch.sigmoid(h)
 
-        g = self.fc1_rel(g)
+        g = self.fc1_entity(g)
         g = F.elu(g)
-        g = self.fc2_rel(g)
-        g = F.elu(g)
-        g = self.fc3_rel(g)
+        # g = self.fc2_entity(g)
+        # g = F.elu(g)
+        g = self.fc3_entity(g)
         pred_g = torch.sigmoid(g)
+
         return pred_h.squeeze(), pred_g.squeeze()
 
     def loss(self, preds, y):
@@ -86,7 +91,7 @@ class Discriminator(nn.Module):
         return loss
 
     def loss_entity_embeddings(self, h, edge_idx, edge_type):
-        z = self.sample_gaussian()
+        z = self.sample_gaussian(h.shape[0])
         preds_real = self.pred_entity(z)
         preds_fake = self.pred_entity(h.detach())
         loss_real = self.real_loss(preds_real)
@@ -94,7 +99,7 @@ class Discriminator(nn.Module):
         return (loss_real + loss_fake) / 2
 
     def loss_relation_embeddings(self, g, edge_idx, edge_type):
-        z = self.sample_gaussian()
+        z = self.sample_gaussian(g.shape[0])
         preds_real = self.pred_rel(z)
         preds_fake = self.pred_rel(g.detach())
         loss_real = self.real_loss(preds_real)
@@ -109,10 +114,10 @@ class Discriminator(nn.Module):
     def discriminator_loss(self, h, g, edge_idx, edge_type):
         loss_g = self.loss_relation_embeddings(g, edge_idx, edge_type)
         loss_h = self.loss_entity_embeddings(h, edge_idx, edge_type)
-        return (loss_g + loss_h)/2
+        return (loss_g + loss_h) / 2
 
-    def sample_gaussian(self):
-        z = torch.randn((self.x_size, self.embedding_size)).to(self.dev)
+    def sample_gaussian(self,n):
+        z = self.distribution.sample((n,)).to(self.dev)
         return z.detach()
 
     def adversarial_loss(self, preds_fake):

train_encoder.py
@@ -15,6 +15,7 @@ from utilis import save_best_encoder, set_random_seed, save_model, ENCODER, KBAT
 
 
 def train_encoder(args, model, data_loader):
+    train_discriminator = 0
     model_name = args.model + "_encoder_final_experiments"
     # set up weights and biases
     if args.debug == 1:
@@ -38,7 +39,7 @@ def train_encoder(args, model, data_loader):
     use_partial = args.use_partial == 1
     use_adversarial = args.use_adversarial == 1
     use_simple_relation = args.use_simple_relation == 1
-
+    last_loss = 100.
     # encoder save file path
     dataset_name = data_loader.get_name()
     encoder_file = ENCODER + '_' + args.model.lower() + '_' + dataset_name.lower() + '.pt'
@@ -131,7 +132,7 @@ def train_encoder(args, model, data_loader):
             torch.cuda.empty_cache()
 
             # discriminator loss
-            if use_adversarial:
+            if use_adversarial and (epoch + 1) >= train_discriminator:
                 loss_D = discriminator.discriminator_loss(h_prime, g_prime, edge_idx, edge_type)
 
             if use_adversarial:
@@ -153,16 +154,19 @@ def train_encoder(args, model, data_loader):
             # optimization
             s_optim = time.time()
 
-            # generator
-            if use_adversarial:
-                optimizer_D.zero_grad()
-                loss_D.backward()
-                optimizer_D.step()
-
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             t_optim = time.time()
+
+            # generator
+            if use_adversarial and (epoch + 1) >= train_discriminator and (last_loss > 0.5 or last_loss > loss.item()):
+                optimizer_D.zero_grad()
+                loss_D.backward()
+                optimizer_D.step()
+
+            if use_adversarial and (epoch + 1):
+                last_loss = loss_D.item()
 
             losses_epoch.append(loss.item())
 
@@ -176,7 +180,8 @@ def train_encoder(args, model, data_loader):
                       'Optim time: %.2f ' % (t_optim - s_optim) +
                       'Loss Graph: %.4f ' % (loss_graph.item()) +
                       'Loss Encoder: %.4f ' % (losses_epoch[-1]) +
-                      ('Loss Discriminator: %.4f ' % (loss_D.item()) if use_adversarial else ''))
+                      ('Loss Discriminator: %.4f ' % (loss_D.item()) if use_adversarial and (
+                              epoch + 1) >= train_discriminator else ''))
 
         loss_epoch = sum(losses_epoch) / len(losses_epoch)
 
@@ -237,7 +242,7 @@ def main():
     # system parameters
     parser.add_argument("--device", type=str, default='cuda', help="Device to use for training.")
     parser.add_argument("--eval", type=int, default=100, help="After how many epochs to evaluate.")
-    parser.add_argument("--debug", type=int, default=0, help="Debugging mod.")
+    parser.add_argument("--debug", type=int, default=1, help="Debugging mod.")
 
     # training parameters
     parser.add_argument("--epochs", type=int, default=3000, help="Number of training epochs for encoder.")
@@ -257,7 +262,7 @@ def main():
     parser.add_argument("--use_partial", type=int, default=0, help="Use a subsample of paths.")
     parser.add_argument("--use_adversarial", type=int, default=0, help="Use a adversarial training.")
     parser.add_argument("--use_simple_relation", type=int, default=0, help="Use simple relation layer.")
-    parser.add_argument("--backprop_relation", type=int, default=0, help="Backprop to the relation layer.")
+    parser.add_argument("--backprop_relation", type=int, default=1, help="Backprop to the relation layer.")
     parser.add_argument("--backprop_entity", type=int, default=0, help="Backprop to the entity layer.")
 
     # encoder parameters